bbfdd07e0344415301aad0c648b1214c19fd9db3
Soufiane Boursen <dev.sboursen@gmail.com>
2022-12-17 15:29:49 +0100

    Add more tests

--- ruby/solution_spec.rb
+++ ruby/solution_spec.rb
@@ -12,4 +12,40 @@ describe "letter_avatar" do
       expect(result[:color]).to eql(expected[:color])
     end
   end
+  
+  context 'Given a nil first_name' do
+    it 'returns an object with the right initials' do
+      user = { id: 3, first_names: nil, last_name: 'Smith'}
+      expected = {initials: 'AS', color: 'green'} 
+
+      result = letter_avatar(user)
+
+      expect(result[:initials]).to eql(expected[:initials])
+      expect(result[:color]).to eql(expected[:color])
+    end
+  end
+
+  context 'Given first_names and last_name are both nil' do
+    it 'returns an object with the right initials' do
+      user = { id: 3, first_names: nil, last_name: nil}
+      expected = {initials: 'AV', color: 'green'} 
+
+      result = letter_avatar(user)
+
+      expect(result[:initials]).to eql(expected[:initials])
+      expect(result[:color]).to eql(expected[:color])
+    end
+  end
+
+  context 'Given another Id' do
+    it 'returns an object with the right initials' do
+      user = { id: 0, first_names: nil, last_name: nil}
+      expected = {initials: 'AV', color: 'red'} 
+
+      result = letter_avatar(user)
+
+      expect(result[:initials]).to eql(expected[:initials])
+      expect(result[:color]).to eql(expected[:color])
+    end
+  end
 end
